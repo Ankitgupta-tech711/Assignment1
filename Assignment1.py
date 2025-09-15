c66759044ccbd9ae0389757c2f17b5183d172015
Ankitgupta-tech711 <ankitbyjus11@gmail.com>
2025-09-15 23:57:15 +0530

    Add initial code for assignment

---
+++ Assignment1.py
@@ -0,0 +1,12 @@
+a=input("Enter the first number:")
+b=input("Enter the second number:")
+a=float(a)
+b=float(b)
+addition=a+b
+print('addition',addition)
+subtraction=b-a
+print('subtraction',subtraction)
+multiplication=a*b
+print('multiplication',multiplication)
+divison=a/b
+print('division',divison)
